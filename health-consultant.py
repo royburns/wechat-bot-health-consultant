69c224b7128e9e405972cdf11aeb9819c44286ad
Roy T.Burns <roy.burns@163.com>
2018-03-04 13:31:22 +0800

    tuling123.com接口优化

--- health-consultant.py
+++ health-consultant.py
@@ -11,60 +11,94 @@ import threading
 from tempfile import NamedTemporaryFile
 
 logging.basicConfig()
-bot = Bot(cache_path=True, console_qr=True)
+bot = Bot(cache_path=True, console_qr=False)
 bot.enable_puid()
 extensions = ['.jpg', '.png', '.gif']
 
+
 def send_online_notification(name):
-	my_friend = ensure_one(bot.search(name))
-	while True:
-		my_friend.send('I\'m Still Alive!! ' + time.strftime('%y/%m/%d-%H:%M:%S', time.localtime()))
-		time.sleep(3600)
+    print('name = ', name)
+    print(bot.search(name))
+    my_friend = ensure_one(bot.search(name))
+    while True:
+        my_friend.send('I\'m Still Alive!! ' + time.strftime('%y/%m/%d-%H:%M:%S', time.localtime()))
+        time.sleep(3600)
 
 
 @bot.register(bot.self)
 def reply(msg):
-	if msg.text == '1':
-		return 'I\'m Still Alive!! ' + time.strftime('%y/%m/%d-%H:%M:%S', time.localtime())
-	else:
-		return tuling_reply(msg.text, msg.sender.puid)
+    print('reply_self')
+    if msg.text == '1':
+        return 'I\'m Still Alive!! ' + time.strftime('%y/%m/%d-%H:%M:%S', time.localtime())
+    else:
+        return tulingChat(msg.text, msg.sender.puid)
+
+
+# 打印来自其他好友、群聊和公众号的消息，并回复
+@bot.register()
+def reply_others(msg):
+    print('reply_others')
+    print(msg.raw)
+    content = re.sub('@[^\s]*', '', unicodedata.normalize('NFKC', msg.text)).strip()
+    print(content)
+    if msg.type == 'Picture':
+        try:
+            res = requests.get(emotions_reply(content[:-4]), allow_redirects=False)
+            tmp = NamedTemporaryFile()
+            tmp.write(res.content)
+            tmp.flush()
+            media_id = bot.upload_file(tmp.name)
+            tmp.close()
+
+            msg.reply_image('.gif', media_id=media_id)
+        except Exception as error:
+            print(error)
+            msg.reply("本机器人没有找到相关表情~使用文字回复：\n" + tuling_reply(content, msg.member.puid))
+    else:
+        return tulingChat(msg.text, msg.sender.puid)
 
 
 @bot.register(Group, SYSTEM, except_self=False)
 def incoming_student(msg):
-	# print('haha')
-	print(msg)
-	print(msg.raw)
-	# if u'加入' in msg.text:
-	# 	msg.reply(tuling_reply('welcome'.encode('UTF-8'), msg.member.puid))
-	# 	msg.reply_image('welcome.jpg')
+    # print('haha')
+    print(msg)
+    print(msg.raw)
+    # if u'加入' in msg.text:
+    #     msg.reply(tuling_reply('welcome'.encode('UTF-8'), msg.member.puid))
+    #     msg.reply_image('welcome.jpg')
+
 
 @bot.register(Group, TEXT)
 def group_msg(msg):
-	if msg.is_at:
-		content = re.sub('@[^\s]*', '', unicodedata.normalize('NFKC', msg.text)).strip()
+    print('group_msg')
+    print(msg)
+    if msg.is_at:
+        content = re.sub('@[^\s]*', '', unicodedata.normalize('NFKC', msg.text)).strip()
+        print(content)
 
-		if content.endswith(tuple(extensions)):
-			try:
-				res = requests.get(emotions_reply(content[:-4]), allow_redirects=False)
-				tmp = NamedTemporaryFile()
-				tmp.write(res.content)
-				tmp.flush()
-				media_id = bot.upload_file(tmp.name)
-				tmp.close()
+        if content.endswith(tuple(extensions)):
+            try:
+                res = requests.get(emotions_reply(content[:-4]), allow_redirects=False)
+                tmp = NamedTemporaryFile()
+                tmp.write(res.content)
+                tmp.flush()
+                media_id = bot.upload_file(tmp.name)
+                tmp.close()
 
-				msg.reply_image('.gif', media_id=media_id)
-			except Exception as error:
-				print(error)
-				msg.reply("本机器人没有找到相关表情~使用文字回复：\n" + tuling_reply(content, msg.member.puid))
-		else:
-			try:
-				msg.reply(apiai_reply(content, msg.member.puid))
-			except Exception as error:
-				print(error)
-				msg.reply(tuling_reply(content, msg.member.puid))
+                msg.reply_image('.gif', media_id=media_id)
+            except Exception as error:
+                print(error)
+                msg.reply("本机器人没有找到相关表情~使用文字回复：\n" + tuling_reply(content, msg.member.puid))
+        else:
+            try:
+                # msg.reply(apiai_reply(content, msg.member.puid))
+                msg.reply(tulingChat(content, msg.member.puid))
+            except Exception as error:
+                print(error)
+                # msg.reply(tuling_reply(content, msg.member.puid))
 
-positiveSendingThread = threading.Thread(target=send_online_notification, args=('乙醚。',))
+
+positiveSendingThread = threading.Thread(target=send_online_notification, args=('小污师',))
 positiveSendingThread.setDaemon(True)
 positiveSendingThread.start()
 
--- messages.py
+++ messages.py
@@ -5,59 +5,90 @@ import requests
 from lxml import etree
 import json
 import random
-import apiai
+# import apiai
+#
+# from api_tokens import *
+# APIAI_TOKEN = 'APIAI_TOKEN'
 
-from api_tokens import *
+TULING_TOKEN = '7abbe8dddc3243429212fae4e8ecc102'
+
 
 def tuling_reply(msg_content, user_id):
-	url_api = 'http://www.tuling123.com/openapi/api'
-	data = {
-		'key'	: TULING_TOKEN,
-		'info'   : msg_content,
-		'userid' : user_id,
-	}
-	
-	print("use Tuling reply")
-	s = requests.post(url_api, data=data).json()
-	print('return code: ' + str(s['code']))
-	if s['code'] == 100000:
-		return s['text']
-	if s['code'] == 200000:
-		return s['text'] + s['url']
-	if s['code'] == 302000:
-		news = random.choice(s['list'])
-		return news['article'] + '\n' + news['detailurl']
-	if s['code'] == 308000:
-		menu = random.choice(s['list'])
-		return menu['name'] + '\n' + menu['detailurl'] + '\n' + menu['info']
+    url_api = 'http://www.tuling123.com/openapi/api'
+    data = {
+        'key': TULING_TOKEN,
+        'info': msg_content.encode('utf-8'),
+        'userid': user_id,
+    }
+    headers = {'Content-type': 'text/html', 'charset': 'utf-8'}
+
+    print("use Tuling reply")
+    # s = requests.post(url_api, data=data).json()
+    s = requests.get(url_api, params=data, headers=headers).json()
+    print('return code: ' + str(s['code']))
+    if s['code'] == 100000:
+        return s['text']
+    if s['code'] == 200000:
+        return s['text'] + s['url']
+    if s['code'] == 302000:
+        news = random.choice(s['list'])
+        return news['article'] + '\n' + news['detailurl']
+    if s['code'] == 308000:
+        menu = random.choice(s['list'])
+        return menu['name'] + '\n' + menu['detailurl'] + '\n' + menu['info']
+    if s['code'] == 40004:
+        return '今天好累了，明天再聊吧😄'
+    if s['code'] == 40007:
+        print('数据格式异常')
+        return '数据格式异常😭'
 
 
-def apiai_reply(msg_content, user_id):
-	print("try APIAI reply...")
-	ai = apiai.ApiAI(APIAI_TOKEN)
-	request = ai.text_request()
-	request.lang = 'zh-CN'
-	request.session_id = user_id
-	request.query = msg_content
+def tulingChat(msg_content, user_id):
+    KEY = '8afba6fdc75544f0bebc465615da1e0b'  # change to your API KEY
+    url = 'http://www.tuling123.com/openapi/api'
+    req_info = msg_content.encode('utf-8')
 
-	response = request.getresponse()
-	s = json.loads(response.read().decode('utf-8'))
-	
-	if s['result']['action'] == 'input.unknown':
-		raise Exception('api.ai cannot reply this message')
-	if s['status']['code'] == 200:
-		print("use APIAI reply")
-		print('return code: ' + str(s['status']['code']))
-		return s['result']['fulfillment']['speech']
+    query = {'key': KEY, 'info': req_info}
+    headers = {'Content-type': 'text/html', 'charset': 'utf-8'}
+
+    try:
+        # 方法一、用requests模块已get方式获取内容
+        r = requests.get(url, params=query, headers=headers)
+        res = r.text
+        result = json.loads(res).get('text').replace('<br>', '\n')
+    except Exception :
+        # result=choice(self.no_answer)
+        result = 'unknow error'
+
+    return result
+
+
+# def apiai_reply(msg_content, user_id):
+#     print("try APIAI reply...")
+#     ai = apiai.ApiAI(APIAI_TOKEN)
+#     request = ai.text_request()
+#     request.lang = 'zh-CN'
+#     request.session_id = user_id
+#     request.query = msg_content
+#
+#     response = request.getresponse()
+#     s = json.loads(response.read().decode('utf-8'))
+#
+#     if s['result']['action'] == 'input.unknown':
+#         raise Exception('api.ai cannot reply this message')
+#     if s['status']['code'] == 200:
+#         print("use APIAI reply")
+#         print('return code: ' + str(s['status']['code']))
+#         return s['result']['fulfillment']['speech']
 
 
 def emotions_reply(keyword):
-	print("try gif reply...")
-	res = requests.get('https://www.doutula.com/search', {'keyword': keyword})
-	html = etree.HTML(res.text)
-	urls = html.xpath('//div[@class="image-container"][1]//img[contains(@class, "img-responsive")]/@data-original')
-	if len(urls) < 1:
-		raise Exception('doutula cannot reply this message')
-	url = 'http:' + random.choice(urls)
+    print("try gif reply...")
+    res = requests.get('https://www.doutula.com/search', {'keyword': keyword})
+    html = etree.HTML(res.text)
+    urls = html.xpath('//div[@class="image-container"][1]//img[contains(@class, "img-responsive")]/@data-original')
+    if len(urls) < 1:
+        raise Exception('doutula cannot reply this message')
+    url = 'http:' + random.choice(urls)
 
-	return url
+    return url
